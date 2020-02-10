a7d368f04fb19c812b74aff5cbe80e404cfe345d
kuldeep6445 <55747743+kuldeep6445@users.noreply.github.com>
2020-02-10 21:17:53 +0530

    initial commit

--- KSBindata.h
+++ KSBindata.h
@@ -3,23 +3,40 @@ using namespace std;
 string find_data(ifstream *myfile , string title ){
 	
 	string total;
-	string track;
+	char track[2];
 	int start =0;
 	int a=0;
 	int b=0;
+	char buffer[50];
 	
 	
 	while(!(myfile->eof())){
-		getline(*myfile , track );
-		total = total +"\n"+ track;
-	}
+		getline(*myfile , total );
+		
+	
 	
 	start = total.find(title);
-	a = total.find(":",start);
-	a = total.find('"',a);
-	b = total.find('"' ,a+1);
-	char buffer[50];
-	total.copy(buffer ,b-a-1,a+1);
+	if(start != std::string::npos){
+	
+		a = total.find(":",start);
+		a = total.find('"',a);
+		b = total.find('"' ,a+1);
+		
+		if(b-a == 3){
+			track[0] = total[a+1];
+			track[1] = total[a+2];
+			return track;
+		}
+		else if(b-a == 2){
+			track[0] = total[a+1];
+			return track;
+		}
+	
+		total.copy(buffer ,b-a-1,a+1);
+	}
+}
 	return buffer;
 	
-}	
+
+	
+}
